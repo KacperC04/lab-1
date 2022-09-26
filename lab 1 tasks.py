0479d2a667d0f96fd220a1848abacc8a66ec342e
KacperC04 <114408326+KacperC04@users.noreply.github.com>
2022-09-26 14:31:13 +0100

    Add files via upload
    lab 1 tasks all done

---
+++ lab 1 tasks.py
@@ -0,0 +1,41 @@
+#task 1
+
+print("hello world")
+
+
+#task 2
+
+print("hello, kacper!)")
+
+#task 3
+
+#Python Program to convert temperature in celsius to fahrenheit
+
+# change this value for a different result
+celsius = 38.4
+
+# calculate fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
+
+#task 4
+
+from statistics import mean
+# list is needed to be created
+inp_lst = [1014, 162,48426]
+list_avg = mean(inp_lst)
+print("Average value of boycotts batting:\n")
+print(list_avg)
+
+
+
+#task 5
+max_students = 24
+total = input("students in groups: ")
+groups = int(total) // 24
+remaing = int(total) % 24
+print("amount of groups has is " + str(groups))
+print("remaining students" + str(remaing))
+
+
+
